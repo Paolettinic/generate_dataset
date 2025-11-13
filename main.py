53ec1b6fda3604bcb0164f44ef08ee9200c9414d
Paolettinic <paolettinic@gmail.com>
2025-11-13 21:42:29 +0100

    Testing model

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 # Load model directly
+import json
 from transformers import AutoTokenizer, AutoModelForCausalLM
+
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+
+
+
+with open("data/prompt.txt", "r") as f:
+    prompt = f.read()
+
+
+with open("./data/text/context_Attrazioni_per_Regione_Molise_103.txt", "r") as f:
+    context = f.read()
+
+with open("./data/dataset_info.jsonl", 'r') as f:
+    info = [json.loads(line) for line in f]
+line = 103
+caption = info[line - 1]['caption']
+
+in_prompt = prompt.format(context=context, caption=caption)
+
 messages = [
     {"role": "user", "content": "Who are you?"},
 ]
+
 inputs = tokenizer.apply_chat_template(
 	messages,
 	add_generation_prompt=True,
@@ -15,4 +35,3 @@ inputs = tokenizer.apply_chat_template(
 ).to(model.device)
 
 outputs = model.generate(**inputs, max_new_tokens=40)
-print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
